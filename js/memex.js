1aebd0ea7684604f6d953b8617699fa557cf2908
akirassasin <AkirAssasin@users.noreply.github.com>
2021-03-26 23:06:06 +0800

    Whoops

--- js/memex.js
+++ js/memex.js
@@ -5,7 +5,7 @@ function documentOnReady () {
     insertElementIntoMemex("In <b>\"As We May Think\"</b>, Vannevar Bush describes a memex as an electromechanical device enabling individuals to develop and read a large self-contained research library, create and follow associative trails of links and personal annotations, and recall these trails at any time to share them with other researchers.", "");
 
     // fetch the memex compiled data
-    fetch('data/memex.mmx').then(response => response.text()).then(text => insertIntoMemex(text));
+    fetch('data/memex.mmx').then(response => response.text()).then(text => insertCompiledIntoMemex(text));
 }
 $(document).ready(documentOnReady);
 
