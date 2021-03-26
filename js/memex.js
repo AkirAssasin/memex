09e16283b4849e0e0a45c8c18979e863effb5f36
akirassasin <AkirAssasin@users.noreply.github.com>
2021-03-26 17:11:11 +0800

    Testing multi-columns

--- js/memex.js
+++ js/memex.js
@@ -2,6 +2,18 @@
 function documentOnReady () {
 
     // fetch the memex compiled data
-    fetch('data/memex.mmx').then(response => response.text()).then(text => console.log(text));
+    fetch('data/memex.mmx').then(response => response.text()).then(text => insertIntoMemex(text));
 }
 $(document).ready(documentOnReady);
+
+// insert memex into container
+function insertIntoMemex (compiled) {
+
+    // split compiled into single lines
+    var lines = compiled.split(/\n/);
+    for (var i = 0; i < lines.length(); ++i) {
+
+        // temporary: insert everything
+        $("#memexContainer").append("<div class=\"memexElement\">" + lines[i] + "</div>");
+    }
+}
